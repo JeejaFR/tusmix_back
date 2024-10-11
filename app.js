3a79d12fd9c928f44f552cef5f64f8adf1dade3a
EJOYEUX <estelle.joyeuxbouillon@hardis-group.com>
2024-10-11 10:04:15 +0200

    multiplayer tusmo init

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected: '+socket.id);
-  io.emit('message', 'Salut mon boeuf');
+const secretWord = "tusmo";
 
-  socket.on('message', (data) => {
-    console.log(data);
-  })
+io.on("connection", (socket) => {
+  console.log("Un joueur s'est connecté");
+
+  socket.on("guess", (data) => {
+    const guessedWord = data.word.toLowerCase();
+    if (guessedWord === secretWord) {
+      io.emit("guessResult", { result: "Correct ! Vous avez trouvé le mot." });
+    } else {
+      io.emit("guessResult", { result: "Faux ! Essayez encore." });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Un joueur s'est déconnecté");
+  });
 });
 
 server.listen(4000, () => {
-  console.log('server running at http://localhost:4000');
+  console.log("server running at http://localhost:4000");
 });
